a3418fd175e050ea31489fae3f0013313ac0d066
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-15 09:01:44 -0500

    Angular learning again

--- ReactProject_simplyLearn/StreamifyApp/streamify/src/components/Movie.jsx
+++ ReactProject_simplyLearn/StreamifyApp/streamify/src/components/Movie.jsx
@@ -4,11 +4,12 @@ import '../App.css';
 
 const Movie = ({ movie }) => {
   return (
-    <Card className="mb-3" style={{ width: "18rem" }}>
+    <Card className="mb-3" style={{ width: "20rem" }}>
       <Card.Img variant="top" src={movie.poster} alt={movie.title} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.description}</Card.Text>
+        <Card.Rating>{movie.rating}</Card.Rating>
       </Card.Body>
     </Card>
   );
