001da797a31cbf36b035c8b308a70089e392d1b2
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-15 12:27:52 -0500

    alert message withstyles message on submit form

--- Angular_Project/welcome-app/src/app/tdf-login/tdf-login.component.ts
+++ Angular_Project/welcome-app/src/app/tdf-login/tdf-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tdf-login',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./tdf-login.component.css']
 })
 export class TdfLoginComponent {
+
+  message: string = "";
+  messageColor:  string = "";
+
+  checkUser(loginRef: NgForm) : void {
+    // alert("Form submitted");
+    console.log("Event fired");
+    console.log(loginRef.value);
+    alert(JSON.stringify(loginRef.value ));
+    let login = loginRef.value;
+    if(login.emailId == "chandu@email.com" && login.password == "123"){
+      this.message = `Login Successful for ${login.emailId}!`;
+      this.messageColor = "green";
+      alert(`Success: Login Successful for ${login.emailId}!`);
+
+    } else {
+      this.message = `Login Failed for ${login.emailId}.`;
+      this.messageColor = "red";
+      alert(`Error: Login Failed for ${login.emailId}.`);
+    }
+    loginRef.reset();
+  }
 
 }
