8e3278b63b1d28330a6ce956bc6e1aa6b61cd2aa
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-15 11:44:51 -0500

    Simple template Driven Form

--- Angular_Project/welcome-app/src/app/template-reference/template-reference.component.ts
+++ Angular_Project/welcome-app/src/app/template-reference/template-reference.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./template-reference.component.css']
 })
 export class TemplateReferenceComponent {
+ message: string = ""
+
+passValue (nameRef: any) : void {
+  let name: string = nameRef.value
+  alert("Why do you clicked me??? : " + name)
+}
+
+checkUser(a: any, b: any):  void {
+  let emailId = a.value;
+  let passowrd = b.value;
+
+  if(emailId == "chandu@email.com" && passowrd == "123"){
+    this.message = "success"
+  } else {
+    this.message = "failure"
+  }
+  a.value=""    // we want reset value in text field 
+  b.value=""
+}
 
 }
