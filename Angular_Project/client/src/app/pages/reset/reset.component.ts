0f11fc1552bb2dfdf2f00a3f5ef71be317448797
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-16 16:16:34 -0500

    basic navbar

--- Angular_Project/client/src/app/pages/reset/reset.component.ts
+++ Angular_Project/client/src/app/pages/reset/reset.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
   templateUrl: './reset.component.html',
   styleUrl: './reset.component.css'
 })
-export class ResetComponent {
+export default class ResetComponent {
 
 }
