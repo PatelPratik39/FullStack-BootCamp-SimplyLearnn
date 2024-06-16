eebce443362b689ef4ee3d11cbfb1849047694d5
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-16 10:43:44 -0500

    Update app.module.ts to include the AppRoutingModule:

--- Angular_Project/task-manager/src/app/app.module.ts
+++ Angular_Project/task-manager/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, TaskListComponent, TaskDetailComponent, UserListComponent, UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
