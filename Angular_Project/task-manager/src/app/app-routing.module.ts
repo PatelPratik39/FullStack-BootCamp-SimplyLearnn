b58abd3b1c3095021576930ae820cb87fe9bbff1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-16 10:42:14 -0500

    routes are configured

--- Angular_Project/task-manager/src/app/app-routing.module.ts
+++ Angular_Project/task-manager/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { UserListComponent } from './users/user-list/user-list.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/tasks', pathMatch:'full'},
+  {path:'tasks', component:TaskListComponent},
+  {path:'tasks/:id', component: TaskDetailComponent},
+  {path:'users', component: UserListComponent},
+  { path: 'users/:id', component: UserDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
