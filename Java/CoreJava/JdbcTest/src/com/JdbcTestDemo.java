23a5f708cd15b85f0504b99fe1afaaf7a59248a1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-03 12:46:35 -0500

    Update, Delete and Insert function using JDBC Driver

--- Java/CoreJava/JdbcTest/src/com/JdbcTestDemo.java
+++ Java/CoreJava/JdbcTest/src/com/JdbcTestDemo.java
@@ -11,14 +11,42 @@ public class JdbcTestDemo {
 			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/my_db", "root","Pratik39");
 			System.out.println("Connection Succesfull!!");
 			Statement stmt = connection.createStatement();
+			/*
+			 * Insert Query
+			 */
 			
-			int result = stmt.executeUpdate("insert into employee values(6,'Chandu Champion',65000)");
+//			int result = stmt.executeUpdate("insert into employee values(7,'Raman Ganga',15000)");
+//			
+//			if(result > 0) {
+//				System.out.println("Record inserted successfully!!");
+//			}
 			
+//			/*
+//			 * Delete Query
+//			 */
+//			
+//			int result = stmt.executeUpdate("delete from employee where id = 2");
+//			if(result > 0) {
+//				System.out.println("Record Deleted Successfully");
+//			} else {
+//				System.out.println("Record Not Present!!");
+//			}
+			
+			
+			/*
+			 * Update Query
+			 */
+			
+			int result = stmt.executeUpdate("Update employee set salary= 35000 where id = 7");
 			if(result > 0) {
-				System.out.println("Record inserted successfully!!");
+				System.out.println("Record Updated Successfully");
+			} else {
+				System.out.println("Record Not Present!!");
 			}
-			stmt.close();
+			stmt.close(); 
+			
 			connection.close();
+			
 		}catch(Exception e) {
 			System.err.println(e);
 		}
