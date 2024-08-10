758f68d6069b35761f443c829ba3cc502ba6d5f2
PatelPratik39 <ptl.pratik717@gmail.com>
2024-08-10 12:15:09 -0500

    simple loginservelet

---
+++ Java/CoreJava/J2EE/SimpleWebApp/src/main/java/com/SignIn.java
@@ -0,0 +1,45 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class SignIn extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public SignIn() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		String emailid = request.getParameter("emailid");  // receiving text field value
+		String password = request.getParameter("password");
+		if(emailid.equals("xyz@gmail.com") && password.equals("123")) {
+			pw.print("Successfuly login with get");
+		}else {
+			pw.println("failure try once again with get");
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		String emailid = request.getParameter("emailid");  // receiving text field value
+		String password = request.getParameter("password");
+		if(emailid.equals("xyz@gmail.com") && password.equals("123")) {
+			pw.print("Successfuly login with get");
+		}else {
+			pw.println("failure try once again with get");
+		}
+	}
+
+}
