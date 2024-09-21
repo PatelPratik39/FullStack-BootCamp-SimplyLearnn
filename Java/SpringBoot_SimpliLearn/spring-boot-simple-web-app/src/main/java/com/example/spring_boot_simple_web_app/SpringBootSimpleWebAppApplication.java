f463a8328260a2391de5de90015fd8e2334a5f31
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-21 12:03:25 -0500

    new changes

---
+++ Java/SpringBoot_SimpliLearn/spring-boot-simple-web-app/src/main/java/com/example/spring_boot_simple_web_app/SpringBootSimpleWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_simple_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleWebAppApplication.class, args);
+	}
+
+}
---
+++ Java/SpringBoot_SimpliLearn/spring-boot-simple-web-app/src/test/java/com/example/spring_boot_simple_web_app/SpringBootSimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_simple_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
